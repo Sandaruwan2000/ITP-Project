591dbdc8f55864adeb26c4445ef77f626bb21f3a
Lahiru Sandaruwan Jayawardana <147098751+Sandaruwan2000@users.noreply.github.com>
2024-01-29 16:15:31 +0530

    create signin api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,9 +1,11 @@
 
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../util/error.js';
+import jwt from 'jsonwebtoken';
 
 
-export const signup =async (req, res) => {
+export const signup =async (req, res ,next) => {
 
     const {username, email, password} = req.body;
     const hashesPassword =bcrypt.hashSync(password, 10);
@@ -12,7 +14,29 @@ export const signup =async (req, res) => {
     try{
    await newUser.save();
    res.status(201).json('User created successfully!');
-}catch(err){
-    res.status(500).json(err.message);
+}catch(error){
+    next(error);
 }
+};
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found!'));
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: pass, ...rest }= validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true})
+        .status(200)
+        .json(rest);
+
+
+    }catch(error){
+        next(error) ;
+    }
+
 };
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import{ useState } from 'react';
+import { Link ,useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+const[formData,setFormData] = useState({});
+const[error,setError] = useState(null);
+const[loading,setLoading] = useState(false);
+const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value});
+  };
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+    setLoading(true);
+    const res = await fetch('/api/auth/signup',
+    {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+
+    });
+    const data = await res.json();
+    console.log(data);
+    if(data.success === false){
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
+  }catch(error){
+    setLoading(false);
+    setError(error.message);
+  }
+   
+    
+  };
+ 
+
   return (
-    <div>Signup</div>
-  )
+    <div className='p-3 max-w-lg mx-auto'>
+
+      <h1 className='text-3xl text-center font-semibold my-7'>Signup</h1>
+      <form  className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="text" placeholder='username' id="username"
+         className='border p-3 rounded' onChange={handleChange} />
+        <input type="email" placeholder='email' id="email" 
+        className='border p-3 rounded' onChange={handleChange}/>
+        <input type="password" placeholder='password' 
+        className='border p-3 rounded' id="password" onChange={handleChange}/>
+        <button disabled={loading} className=' bg-slate-500 text-white p-3  rounded-lg uppercase hover:opacity-50 disabled:opacity-80' id="signin">
+          {loading ? 'Loading...' : 'Signup'}
+          </button>
+      </form>
+
+      <div className='flex gap-3 mt-5'> 
+        <p>Have an account?</p>
+        <Link to='/sign-in' className='text-blue-700 hover:underline'>Signin</Link>
+
+      </div>
+
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      </div>
+  );
 }
